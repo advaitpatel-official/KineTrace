8dbfc8336c4d05c36af5bbae38a975548edc5169
advaitpatel-official <advait.patel@outlook.com>
2026-07-18 20:12:49 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -277,7 +277,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://kinetrace.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
