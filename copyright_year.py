278b102a7ae70df7d148269d3c0c6cdc194392b6
advaitpatel-official <advait.patel@outlook.com>
2026-07-22 21:05:34 -0400

    test

--- copyright_year.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def get_copyright_year() -> int:
-    return datetime.now().year
-
-def get_copyright_notice(name: str = "Advait Patel") -> str:
-    return f"\u00a9 {name}, {get_copyright_year()}"
